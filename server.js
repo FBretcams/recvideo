6fb8f1516a9c49e7dbfc14fa9bc927a574caa4d0
FBretcams <ferbreton66@gmail.com>
2022-01-28 00:10:51 -0600

    add

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-app.get('/public', (req, res) => res.send('hola mundo'))
+app.get('/', (req, res) => res.send('hola mundo'))
 
 app.listen(3000);
 console.log('port 3000')
