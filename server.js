ff7dfa7ea3f81372dc78b7c5188e23f0082c8877
FBretcams <98174920+FBretcams@users.noreply.github.com>
2022-02-28 09:32:30 -0600

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => res.send('TE AMO RUBI <3'))
+app.get('/', (req, res) => res.send(''))
 
 app.listen(3000);
 console.log('port 3000')
